42ac1d0afd641433663c4de3cad2f1cbd6f028c4
Christopher Gedler <45577870+cgedler@users.noreply.github.com>
2021-03-07 11:32:13 -0400

    Create __init__.py

---
+++ barcode/admin/__init__.py
@@ -0,0 +1,21 @@
+"""
+======================== Copyright 2020 CORPYGEDLER ==================================
+--  Author: Christopher Gedler <cgedler@gmail.com;cge.systemsolutions@gmail.com>
+--  Create date: 2020-05-28
+--  Description: __init__.py
+--
+--  Import files in this Folder
+======================================================================================
+"""
+#
+# Imports :
+#
+from .adestino import *
+from .aguia import *
+from .aorigen import *
+from .abultos import *
+from .ausersuc import *
+from .aruta import *
+from .acamion import *
+from .acliente import *
+from .aemail import *
