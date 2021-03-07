3679eeac685877cd9fc5a8c2a8b1f5b22886e591
Christopher Gedler <45577870+cgedler@users.noreply.github.com>
2021-03-07 11:50:54 -0400

    Create __init__.py

---
+++ barcode/urls_views/__init__.py
@@ -0,0 +1,21 @@
+"""
+======================== Copyright 2020 CORPYGEDLER ==================================
+--  Author: Christopher Gedler <cgedler@gmail.com;cge.systemsolutions@gmail.com>
+--  Create date: 2020-05-28
+--  Description: __init__.py
+--
+--  Import files in this Folder
+======================================================================================
+"""
+#
+# Imports :
+#
+from .ubase import *
+from .udestino import *
+from .uguia import *
+from .uorigen import *
+from .uruta import *
+from .ubultos import *
+from .ucamion import *
+from .ucliente import *
+from .uemails import *
