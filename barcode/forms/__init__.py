03be5dba5f240ca9818d4ba984ad43169b5a94ee
Christopher Gedler <45577870+cgedler@users.noreply.github.com>
2021-03-07 11:35:02 -0400

    Create __init__.py

---
+++ barcode/forms/__init__.py
@@ -0,0 +1,18 @@
+"""
+======================== Copyright 2020 CORPYGEDLER ==================================
+--  Author: Christopher Gedler <cgedler@gmail.com;cge.systemsolutions@gmail.com>
+--  Create date: 2020-05-28
+--  Description: __init__.py
+--
+--  Import files in this Folder
+======================================================================================
+"""
+#
+# Imports :
+#
+from .fdestino import *
+from .fguia import *
+from .forigen import *
+from .fruta import *
+from .fcamion import *
+from .fcliente import *
