14a5c75298165346928d8475a8198084a91dfbdd
Christopher Gedler <45577870+cgedler@users.noreply.github.com>
2021-03-07 11:37:24 -0400

    Create __init__.py

---
+++ barcode/models/__init__.py
@@ -0,0 +1,21 @@
+"""
+======================== Copyright 2020 CORPYGEDLER ==================================
+--  Author: Christopher Gedler <cgedler@gmail.com;cge.systemsolutions@gmail.com>
+--  Create date: 2020-05-28
+--  Description: __init__.py
+--
+--  Import files in this Folder
+======================================================================================
+"""
+#
+# Imports :
+#
+from .mdestino import *
+from .mguia import *
+from .morigen import *
+from .mbultos import *
+from .musersuc import *
+from .mruta import *
+from .mcamion import *
+from .mcliente import *
+from .memail import *
