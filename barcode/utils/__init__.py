eabd3383d4e228dab8091f9444598ddad2deaf34
Christopher Gedler <45577870+cgedler@users.noreply.github.com>
2021-03-07 11:59:06 -0400

    Create __init__.py

---
+++ barcode/utils/__init__.py
@@ -0,0 +1,14 @@
+"""
+======================== Copyright 2020 CORPYGEDLER ==================================
+--  Author: Christopher Gedler <cgedler@gmail.com;cge.systemsolutions@gmail.com>
+--  Create date: 2020-05-28
+--  Description: __init__.py
+--
+--  Import files in this Folder
+======================================================================================
+"""
+#
+# Imports :
+#
+from .renderpdf import *
+from .cEmpresa import *
