97d0bea2eac0569d69265a40dc48e3d020c2906a
Christopher Gedler <45577870+cgedler@users.noreply.github.com>
2021-03-07 12:00:08 -0400

    Create __init__.py

---
+++ barcode/views/__init__.py
@@ -0,0 +1,22 @@
+"""
+======================== Copyright 2020 CORPYGEDLER ==================================
+--  Author: Christopher Gedler <cgedler@gmail.com;cge.systemsolutions@gmail.com>
+--  Create date: 2020-05-28
+--  Description: __init__.py
+--
+--  Import files in this Folder
+======================================================================================
+"""
+#
+# Imports :
+#
+from .vbase import *
+from .vdestino import *
+from .vguia import *
+from .vorigen import *
+from .vbultos import *
+from .vruta import *
+from .vbulcarga import *
+from .vcamion import *
+from .vcliente import *
+from .vemails import *
